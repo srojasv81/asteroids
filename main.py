44a7a5a0909f99015639ad8fdce90cc86e938275
srojasv81 <srojasv@gmail.com>
2024-12-14 11:15:46 -0300

    ch2-5 Draw Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     # chapter 2 exercise 3
@@ -10,6 +11,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = reloj.tick(60) / 1000
         
