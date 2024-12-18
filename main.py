19fbef844da15c667e3e03d326ebedb7486e090a
srojasv81 <srojasv@gmail.com>
2024-12-18 12:44:17 -0300

    ch3-5 Destruction

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
@@ -50,6 +55,5 @@ def main():
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
         
-
 if __name__ == "__main__":
     main()
